080cc96362b96dd8f3df32cf933f72d66aebc336
zeffur <zeffurs@gmail.com>
2019-05-17 23:48:26 +0700

    Added cases of help & quit in main menu

--- src/menu.cpp
+++ src/menu.cpp
@@ -7,38 +7,7 @@
 
 using namespace std;
 
-/*int SecMenu() {
-
-    cout << "Select difficulty level:" << endl;
-    cout << "1.Easy" << endl;
-    cout << "2.Normal" << endl;
-    cout << "3.Hard" << endl;
-    cout << "4.Go Back" << endl;
-    cout << endl;
-    int k = 0;
-    cin >> k;
-    switch (k) {
-    case 1:
-      File1();
-      break;
-    case 2:
-      File2();
-      break;
-    case 3:
-      File3();
-      break;
-    case 4:
-      return 1;
-    default:
-      cout << "Error\n";
-      break;
-    }*/
-
-//  return 0;
-//}*/
-
 int Menu() {
-  //  int l = 0, k = 0;
   if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0) {
     printf("error initializing SDL: %s\n", SDL_GetError());
     return 1;
@@ -455,9 +424,9 @@ int Menu() {
           SDL_RenderPresent(rend);
           SDL_Delay(100);
           printf("lol \n");
-          /*SDL_RenderClear(rend);
+          SDL_RenderClear(rend);
           SDL_RenderCopy(rend, tex_help, NULL, NULL);
-          SDL_RenderPresent(rend);*/
+          SDL_RenderPresent(rend);
         }
         if (mouse_x > 447 && mouse_x < 574 && mouse_y > 484 && mouse_y < 537 &&
             status == 1) {
@@ -503,24 +472,6 @@ int Menu() {
         }
       }
     }
-    // SecMenu();
-    // switch ()
-    /* cout << "1.Game" << endl;
-      cout << "2.Help" << endl;
-      cout << "3.Exit" << endl;*/
-    //  cin >> k;
-    /*  switch (k) {
-      case 1:
-        l = SecMenu();
-        break;
-      case 2:
-        l = Help();
-        break;
-      case 3:
-        return 0;
-      }
-      if (l)
-        Menu();*/
   }
 
   SDL_DestroyRenderer(rend);
